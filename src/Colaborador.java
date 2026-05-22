6d51f62d32b1bd21284009915d7f749cc7e57745
danbok <danbokf458@gmail.com>
2026-05-22 13:27:00 -0300

    Remover classe App e adicionar classes Colaborador e Main

---
+++ src/Colaborador.java
@@ -0,0 +1,11 @@
+public class Colaborador {
+    private int registro;
+    private String nome;
+    private String tipo;
+
+     public Colaborador(int registro, String nome, String tipo) {
+        this.registro = registro;
+        this.nome = nome;
+        this.tipo = tipo;
+    }
+}
