fdd695c4a7267d7a3f661298988965ebaa94b2f8
danbok <danbokf458@gmail.com>
2026-06-03 19:05:28 -0300

    Adicionar lógica de cálculo e exibição de salários para colaboradores

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         
         double salarioBase = 2000.0;
         double aumentoBase = salarioBase * 5/100; 
-        System.out.println(aumentoBase);
+        double salarioComAumento = salarioBase + aumentoBase;
         
         System.out.println("Sistema folha de pagamentos");
 
@@ -26,14 +26,14 @@ do {
     switch (opcao) {
         case 1:
             System.out.println("Cadastrar colaborador");
-            System.out.print("Digite o registro do colaborador: ");
-        int registro = scanner.nextInt();
-        scanner.nextLine(); // Limpar o buffer
+            System.out.print("\n" + "Digite o registro do colaborador: ");
+            int registro = scanner.nextInt();
+            scanner.nextLine(); // Limpar o buffer
 
-        System.out.print("Digite o nome do colaborador: ");
+        System.out.print("\n" + "Digite o nome do colaborador: ");
         String nome = scanner.nextLine();
 
-        System.out.print("Digite o tipo do colaborador: \n"); 
+        System.out.print("\n" + "Digite o tipo do colaborador: \n"); 
 
         System.out.println( "\n Tipos: \n 1 - padrão \n 2 - comissionado \n 3 - de produção");     
 
@@ -42,11 +42,17 @@ do {
         Colaborador colaborador = new Colaborador(registro, nome, opcaoTipo);
 
         if (opcaoTipo == 1) {
-            // Lógica para colaborador padrão
+            System.out.println("Colaborador padrão salvo com sucesso. \n Salário base: " + salarioBase + "\n Aumento: " + aumentoBase + "\n Salário com aumento: " + salarioComAumento);
         } else if (opcaoTipo == 2) {
-            // Lógica para colaborador comissionado
+            System.out.println("Qual o valor das vendas do colaborador comissionado? ");
+            double valorVendas = scanner.nextDouble();
+            scanner.nextLine(); // Limpar o buffer
+            System.out.println("Colaborador comissionado salvo com sucesso. \n Salário: " + (salarioComAumento + valorVendas));
         } else if (opcaoTipo == 3) {
-            // Lógica para colaborador de produção
+            System.out.println("Número de peças produzidas pelo colaborador de produção: ");
+            int pecasProduzidas = scanner.nextInt();
+            scanner.nextLine(); // Limpar o buffer
+            System.out.println("Colaborador de produção salvo com sucesso. \n Salário: " + (salarioComAumento + (pecasProduzidas * 10)));
         }
 
         colaboradores.add(colaborador);
@@ -55,6 +61,8 @@ do {
 
         case 2:
             System.out.println("Listar colaboradores");
+            System.out.println("Colaboradores cadastrados: " + colaboradores.size() + "\n");
+            colaboradores.forEach(c -> System.out.println("Colaborador: " + c.getNome() + "\n" + "Tipo: " + c.getTipo() + "\n" + "Registro: " + c.getRegistro() + "\n" + "-----------------------------"));
             break;
 
         case 3:
@@ -67,10 +75,6 @@ do {
     }
 
 } while (opcao != 3);
-
-        for (Colaborador c : colaboradores) {
-            System.out.println("Colaborador: " + c.getNome() + "\n" + "Tipo: " + c.getTipo() + "\n" + "Registro: " + c.getRegistro());
-        }
 
          scanner.close();
     }
